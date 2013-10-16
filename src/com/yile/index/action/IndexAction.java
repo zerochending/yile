3ff25604d7a53c09aa3ee993e79288cbe972c564
unknown <adrainding@adrainding-PC0.tencent.com>
2013-10-16 18:13:03 +0800

    yile start!

---
+++ src/com/yile/index/action/IndexAction.java
@@ -0,0 +1,21 @@
+package com.yile.index.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.yile.base.BaseAction;
+
+
+public class IndexAction extends BaseAction{
+	
+	public static final Logger log = Logger.getLogger(IndexAction.class); 
+	
+	public ModelAndView appHotRank(HttpServletRequest request, HttpServletResponse response){
+		
+		return null;
+		
+	}
+}
